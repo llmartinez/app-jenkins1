c0f76c26d186e88cb110033b2d22d634d08ab901
Roger Guasch <rguasch@grupeina.com>
2014-01-23 14:06:37 +0100

    Version 0.1 de User con login, eliminado todo lo demas

---
+++ src/Adservice/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Adservice\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class DefaultController extends Controller
+{
+    
+    public function indexAction()
+    {
+        echo "hola";die;
+    }
+}
