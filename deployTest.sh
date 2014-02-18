1da398d9a66b690a0b63977730d0f08e7837fee0
Daniel Maya <dmaya@grupeina.com>
2014-02-18 10:11:46 +0100

    jenkins config problems

--- deployTest.sh
+++
@@ -1,8 +0,0 @@
-clear
-echo '
-  Reloading schema "ad-service-test" and fixtures: 
-';
-
-php app/console doctrine:schema:drop --force
-php app/console doctrine:schema:create 
-php app/console d:f:l
