3436c854b855e00d7ac8079e37483014870f2c7c
Daniel Maya <dmaya@grupeina.com>
2014-02-28 11:23:26 +0100

    testEditPartner

--- src/Adservice/PartnerBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Adservice/PartnerBundle/Tests/Controller/DefaultControllerTest.php
@@ -44,6 +44,45 @@ class DefaultControllerTest extends WebTestCase
     }
 
     /**
+     * Test que comprueba que se edite un partner
+     * @dataProvider editPartners
+     */
+    public function testEditPartner($partner)
+    {
+        $client = static::createClient();
+        $client-> followRedirects(true);
+        UtilFunctionTest::doLogin($client, 'admin', 'admin');
+        UtilFunctionTest::linkTo($client, $this, 'table tr td a#partner_list');
+
+        $location = 'table tr td a#edittestpartner';
+        $link = $client->getCrawler()->filter($location)->link();
+        $crawler = $client->click($link);
+
+        //comprueba que vaya a la pagina de edicion de usuarios
+        $this->assertRegExp('/.*\/..\/partner\/edit\/.*/', $client->getRequest()->getUri(),
+            'El usuario ve el listado de usuarios'
+        );
+
+        //carga el form con los datos editados del usuario
+        $editUserForm = $crawler->selectButton('btn_save')->form($partner);
+        //ejecuta el submit del form
+        $crawler = $client->submit($editUserForm);
+
+        //comprueba que devuelva una pagina sin error
+        $this->assertTrue($client->getResponse()->isSuccessful());
+
+        //comprueba que vuelva a la pagina del listado de usuarios
+        $this->assertRegExp('/.*\/..\/partner\/list/', $client->getRequest()->getUri(),
+            'El usuario ve el listado de partners'
+        );
+
+        $this->assertGreaterThan(0, $crawler->filter('table tr td:contains("testpartner_edited@test.es")')->count(),
+            'Se ha editado el mail del partner');
+        //volver al inicio
+        UtilFunctionTest::linkTo($client, $this, 'ol li a:contains("Home")');
+    }
+
+    /**
      * DataProvider de partners: Contiene un array de partners
      * @return array partners
      */
@@ -56,13 +95,28 @@ class DefaultControllerTest extends WebTestCase
                     'adservice_partnerbundle_partnertype[phone_number_1]' => '123456789',
                     'adservice_partnerbundle_partnertype[phone_number_2]' => '123456879',
                     'adservice_partnerbundle_partnertype[fax]'            => '123456789',
-                    'adservice_partnerbundle_partnertype[email_1]'        => 'testadmin@test.es',
-                    'adservice_partnerbundle_partnertype[email_2]'        => 'testadmin@test.es',
-                    'adservice_partnerbundle_partnertype[address]'        => 'testadmin@test.es',
-                    'adservice_partnerbundle_partnertype[postal_code]'    => '00000',
+                    'adservice_partnerbundle_partnertype[email_1]'        => 'testpartner@test.es',
+                    'adservice_partnerbundle_partnertype[email_2]'        => 'testpartner@test.es',
+                    'adservice_partnerbundle_partnertype[address]'        => 'testaddress',
+                    'adservice_partnerbundle_partnertype[postal_code]'    => '98765',
                     'adservice_partnerbundle_partnertype[active]'         => '1',
                     'adservice_partnerbundle_partnertype[region]'         => '1',
                     'adservice_partnerbundle_partnertype[province]'       => '1',
+                    ),
+            ),
+        );
+    }
+
+    /**
+     * DataProvider de partners editados: Contiene dos campos de email a editar para un partner
+     * @return array editPartners
+     */
+    public function editPartners()
+    {
+        return array(
+            array('partner' => array(
+                    'adservice_partnerbundle_partnertype[email_1]'    => 'testpartner_edited@test.es',
+                    'adservice_partnerbundle_partnertype[email_2]'    => 'testpartner_edited@test.com',
                     ),
             ),
         );
