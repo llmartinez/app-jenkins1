e9b51e89fa8952df78bd36f5de141d16d7135feb
rguasch <rguasch@grupeina.com>
2014-03-03 12:19:41 +0100

    preparacion para demo

--- src/Adservice/UtilBundle/Tests/Entity/ProvinceTest.php
+++ src/Adservice/UtilBundle/Tests/Entity/ProvinceTest.php
@@ -1,34 +1,34 @@
 <?php
-namespace Adservice\UtilBundle\Tests\Entity;
-
-use Adservice\UtilBundle\Entity\Province;
-use Adservice\UtilBundle\Tests\Entity\RegionTest;
-
-class ProvinceTest extends \PHPUnit_Framework_TestCase
-{
-    protected $province;
-    
-    public function testSettersProvince()
-    {
-        $province = new Province();
-        $province->setProvince('ProvinciaTest'); 
-        $province->setRegion(RegionTest::GetRegion()); 
-        
-        $this->province = $province;
-    }
-    
-    public function testGettersProvince()
-    {
-        $province = $this->province;
-        return $province;
-    }
-    
-    public static function GetProvince()
-    {
-        $province = new Province();
-        $province->setProvince('ProvinciaTest'); 
-        $province->setRegion(RegionTest::GetRegion()); 
-        
-        return $province;
-    }
-}
+//namespace Adservice\UtilBundle\Tests\Entity;
+//
+//use Adservice\UtilBundle\Entity\Province;
+//use Adservice\UtilBundle\Tests\Entity\RegionTest;
+//
+//class ProvinceTest extends \PHPUnit_Framework_TestCase
+//{
+//    protected $province;
+//    
+//    public function testSettersProvince()
+//    {
+//        $province = new Province();
+//        $province->setProvince('ProvinciaTest'); 
+//        $province->setRegion(RegionTest::GetRegion()); 
+//        
+//        $this->province = $province;
+//    }
+//    
+//    public function testGettersProvince()
+//    {
+//        $province = $this->province;
+//        return $province;
+//    }
+//    
+//    public static function GetProvince()
+//    {
+//        $province = new Province();
+//        $province->setProvince('ProvinciaTest'); 
+//        $province->setRegion(RegionTest::GetRegion()); 
+//        
+//        return $province;
+//    }
+//}
