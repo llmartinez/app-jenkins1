eebe3bf81bc9ba14972f18a063ae1c9ecf6b6d91
rguasch <rguasch@grupeina.com>
2014-03-03 10:06:39 +0100

    puesta a punto para demo

--- src/Adservice/CarBundle/Form/CarType.php
+++ src/Adservice/CarBundle/Form/CarType.php
@@ -3,13 +3,14 @@ namespace Adservice\CarBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
+use Adservice\CarBundle\Form\BrandType;
 
 
 class CarType extends AbstractType
 {
     public function buildForm(FormBuilder $builder, array $options)
     {
-        $builder
+        $builder 
                 ->add('version')
                 ->add('year', 'text')
                 ->add('vin')
