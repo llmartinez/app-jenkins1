af225b7accb5c251a8afc9c1d10c734c12eac3a8
dmaya <dmaya@grupeina.com>
2014-11-10 18:12:31 +0100

    bugfix: delete QuerySelectors pagination workshop

--- src/Adservice/WorkshopBundle/Resources/public/js/workshop_filter.js
+++ src/Adservice/WorkshopBundle/Resources/public/js/workshop_filter.js
@@ -4,16 +4,12 @@ $(document).ready(function() {
     //REDIRIGE A LA PAGINA EN LA QUE SE HAYA HECHO CLICK
     $('#flt_country').click(function() {
 
-        var country = $('#flt_country').val();
+            var route   = $('#route').val();
+            var country = $('#flt_country').val();
+            if(country == null) country = 'none';
 
-        if(country == null) country = 'none';
-
-        var select = document.querySelector('#flt_country');
-        var data   = select.dataset;
-        var url    = data.url;
-
-        url = url.replace("plc_page", 1);
-        url = url.replace("plc_country", country);
+            var locale = $(document).find("#data_locale").val();
+            var url = Routing.generate(route, {_locale: locale, page: 1, country: country });
 
         window.open(url, "_self");
     });
