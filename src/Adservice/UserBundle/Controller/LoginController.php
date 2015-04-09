26f0bfc60df2137fa3527621aa7608397b84db42
dmaya <dmaya@grupeina.com>
2015-04-09 12:59:18 +0200

    bloquear accesso desde IE

--- src/Adservice/UserBundle/Controller/LoginController.php
+++ src/Adservice/UserBundle/Controller/LoginController.php
@@ -13,15 +13,29 @@ class LoginController extends Controller {
     public function loginAction() {
         $request = $this->getRequest();
         $session = $request->getSession();
-        // obtiene el error de inicio de sesión si lo hay
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        } else {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+
+        $u_agent = $_SERVER['HTTP_USER_AGENT'];
+
+        // Para controlar si es IE11 o superior...
+        if((preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)) || (preg_match("/(Trident\/(\d{2,}|7|8|9)(.*)rv:(\d{2,}))|(MSIE\ (\d{2,}|8|9)(.*)Tablet\ PC)|(Trident\/(\d{2,}|7|8|9))/", $u_agent)))
+        {
+            $bname = 'Internet Explorer';
+            $ub = "MSIE";
         }
 
-        return $this->render('UserBundle:Login:login.html.twig', array('last_username'  => $session->get(SecurityContext::LAST_USERNAME),
-                                                                       'error'          => $error));
+        if(!$this->get('cms.isMSIE')->isMSIE($request)){
+            // obtiene el error de inicio de sesión si lo hay
+            if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+                $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+            } else {
+                $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            }
+
+            return $this->render('UserBundle:Login:login.html.twig', array('last_username'  => $session->get(SecurityContext::LAST_USERNAME),
+                                                                           'error'          => $error));
+        }else{
+            return $this->render('UserBundle:Login:error_explorer.html.twig');
+        }
     }
 
     public function goToLoginAction() {
