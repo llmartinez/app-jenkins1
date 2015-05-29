87df63ce83893fd86bce9d1283479dc34a2b56f7
dmaya <dmaya@grupeina.com>
2015-05-29 09:45:22 +0200

    alerta de usuario repetido no deja editar users

--- src/Adservice/UserBundle/Controller/UserController.php
+++ src/Adservice/UserBundle/Controller/UserController.php
@@ -225,14 +225,29 @@ class UserController extends Controller {
                     $username = UtilController::getUsernameUnused($em, $name);
                     $user->setUsername($username);
 
-                    $error_username = $this->get('translator')->trans('username_used').$username;
+                    if ($username != $name) {
+                        $error_username = $this->get('translator')->trans('username_used').$username;
 
-                    return $this->render('UserBundle:User:edit_user.html.twig', array('user'      => $user,
-                                                                          'form_name' => $form->getName(),
-                                                                          'form'      => $form->createView(),
-                                                                          'error_username' => $error_username));
+                        return $this->render('UserBundle:User:edit_user.html.twig', array('user'      => $user,
+                                                                              'form_name' => $form->getName(),
+                                                                              'form'      => $form->createView(),
+                                                                              'error_username' => $error_username));
+                    }
                 }
+////////////////////////////////////////////////////////////////////////////////////
+///
+            // Si el username ya exite, mostramos un error
+            if ($username != $name) {
 
+                $error_username = $this->get('translator')->trans('username_used').$username;
+
+                return $this->render('UserBundle:User:new_user.html.twig', array(  'user'       => $user,
+                                                                                   'user_type'  => $type,
+                                                                                   'form_name'  => $form->getName(),
+                                                                                   'form'       => $form->createView(),
+                                                                                    'error_username' => $error_username));
+            }
+            ////////////////////////////////////////////////////////////////////////
                 $user = UtilController::settersContact($user, $user, $actual_region, $actual_city);
                 $this->saveUser($em, $user, $original_password);
 
