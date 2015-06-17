b2f8c7e13c6da279a056500cc56cead73cbf3a30
dmaya <dmaya@grupeina.com>
2015-06-17 18:06:59 +0200

    rellenar codigo de socio al editar Workshop

--- src/Adservice/WorkshopBundle/Resources/public/js/workshop_shop.js
+++ src/Adservice/WorkshopBundle/Resources/public/js/workshop_shop.js
@@ -4,6 +4,11 @@ $(document).ready(function() {
     var id_shop = $('form').find('select[id*=_shop]').val();
     populate_shop(id_shop);
 
+    var partner = $('form').find('select[name*=partner]').val();
+    if (partner != '') {
+    	fill_code_partner(partner);
+    }
+
     //si clickamos el combobox de los socios rellenamos el de tiendas
     $('form').find('select[name*=partner]').change(function() {
         populate_shop();
