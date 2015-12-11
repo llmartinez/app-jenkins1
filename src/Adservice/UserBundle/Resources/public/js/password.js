0f87a7622fd22cb4b4dc28463c6d0369d7c2b2d7
dmaya <dmaya@grupeina.com>
2015-12-11 12:09:52 +0100

    Translations + control passwords

--- src/Adservice/UserBundle/Resources/public/js/password.js
+++ src/Adservice/UserBundle/Resources/public/js/password.js
@@ -11,26 +11,31 @@
 
         $('#confirm_password').click(function(){
 
-            var id = $('#user_id').val();
-            var route  = 'change_password';
-            var locale = $(document).find("#data_locale").val();
+            var error = check_password();
 
-            var new_pass = $('#new_password1').val();
-            var rep_pass = $('#new_password2').val();
-            var old_pass = $('#old_password' ).val();
+            if (error == 0) {
+                var id = $('#user_id').val();
+                var route  = 'change_password';
+                var locale = $(document).find("#data_locale").val();
 
-            if(new_pass == '') new_pass = 'none';
-            if(rep_pass == '') rep_pass = 'none';
-            if(old_pass == '') old_pass = 'none';
+                var new_pass = $('#new_password1').val();
+                var rep_pass = $('#new_password2').val();
+                var old_pass = $('#old_password' ).val();
 
-            var url = Routing.generate(route, {_locale: locale, id: id, new_pass: new_pass, rep_pass: rep_pass, old_pass: old_pass });
-            window.open(url,'_self');
+                if(new_pass == '') new_pass = 'none';
+                if(rep_pass == '') rep_pass = 'none';
+                if(old_pass == '') old_pass = 'none';
+
+
+                var url = Routing.generate(route, {_locale: locale, id: id, new_pass: new_pass, rep_pass: rep_pass, old_pass: old_pass });
+                window.open(url,'_self');
+            }
         });
 
 
         function check_password()
         {
-            var password = $("input[id*='_type_password_password1']").val();
+            var password = $("input[id*='_password1']").val();
             var pass = password.toLowerCase();
             var PASS = password.toUpperCase();
 
@@ -49,6 +54,7 @@
             }
             if (find == 0) error = 1;
             else {
+                find = 0;
                 for(i=0; i<pass.length; i++){
                     if (letras.indexOf(pass.charAt(i),0)!=-1){
                         find = 1;
@@ -57,6 +63,7 @@
             }
             if (find == 0) error = 1;
             else {
+                find = 0;
                 for(i=0; i<PASS.length; i++){
                     if (letras.indexOf(PASS.charAt(i),0)!=-1){
                         find = 1;
@@ -64,15 +71,12 @@
                 }
             }
 
+            if (find == 0) error = 1;
+
             if (error == 1) {
                 event.preventDefault();
                 alert($("#badpass").val());
             }
 
-
-            if ( isNaN($("input[id*='number_']").val())) {
-                $("input[id*='number_']").css('border-color','#FF0000');
-                alert($("#isNaN").val());
-                return false;
-            }
+            return error;
         }
