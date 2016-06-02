d606826339779b03b4c9f0760949a39ea46cddd7
dmaya <dmaya@grupeina.com>
2016-06-02 13:48:12 +0200

    ver tickets pendientes sin mostrar los libres

--- src/Adservice/TicketBundle/Controller/TicketController.php
+++ src/Adservice/TicketBundle/Controller/TicketController.php
@@ -155,8 +155,8 @@ class TicketController extends Controller {
             $params[] = array('status', ' = '.$open->getId());
 
             $country_service = $security->getToken()->getUser()->getCountryService()->getId();
-            if($country_service == '7') $params[] = array('id'   , ' != 0 AND e.assigned_to = '.$id_user.' OR (e.assigned_to IS NULL AND w.country IN (5,6) AND e.status = 1)');
-            else                        $params[] = array('id'   , ' != 0 AND e.assigned_to = '.$id_user.' OR (e.assigned_to IS NULL AND w.country = '.$country_service.' AND e.status = 1)');
+            if($country_service == '7') $params[] = array('id'   , ' != 0 AND e.assigned_to = '.$id_user.'');// OR (e.assigned_to IS NULL AND w.country IN (5,6) AND e.status = 1)');
+            else                        $params[] = array('id'   , ' != 0 AND e.assigned_to = '.$id_user.'');// OR (e.assigned_to IS NULL AND w.country = '.$country_service.' AND e.status = 1)');
         }
 
         elseif ($option == 'assessor_answered')
