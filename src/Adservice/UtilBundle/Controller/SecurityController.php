4a5f2a63601e415f1ddc6131d11258e5abbf718e
dmaya <dmaya@grupeina.com>
2016-06-07 09:25:22 +0200

    habilitar autologin (demo)

--- src/Adservice/UtilBundle/Controller/SecurityController.php
+++ src/Adservice/UtilBundle/Controller/SecurityController.php
@@ -26,14 +26,14 @@ class SecurityController extends Controller{
         ///////////////////////////////////////////////////////////////////////////////////////
         // Mostrar Token encriptado para test
         ///////////////////////////////////////////////////////////////////////////////////////
-        // $user = $em->getRepository('UserBundle:User')->findOneById(4165); //danisnachezripoll
-        // $tok = $user->getToken();
-        // $enc = $this->encryptADS($tok);
-        // $dec = $this->decryptADS($enc);
-        // var_dump('Token: '.$tok);
-        // var_dump('Encript: '.$enc);
-        // var_dump('Decript => ');
-        // var_dump($dec);die;
+        $user = $em->getRepository('UserBundle:User')->findOneById(3318); //adpruebas
+        $tok = $user->getToken();
+        $enc = $this->encryptADS($tok);
+        $dec = $this->decryptADS($enc);
+        var_dump('Token: '.$tok);
+        var_dump('Encript: '.$enc);
+        var_dump('Decript => ');
+        var_dump($dec);die;
         ///////////////////////////////////////////////////////////////////////////////////////
 
     	if($token != null)
