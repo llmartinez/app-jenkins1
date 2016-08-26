f4379464ab907b5bbc6e2dda32c15b81cb6eff4d
dmaya <dmaya@grupeina.com>
2016-08-26 14:53:28 +0200

    Bugfix:paginacion del listado de tiendas (SUPERADMIN)

--- src/Adservice/OrderBundle/Controller/WorkshopOrderController.php
+++ src/Adservice/OrderBundle/Controller/WorkshopOrderController.php
@@ -349,7 +349,7 @@ class WorkshopOrderController extends Controller {
         }
         $user = $security->getToken()->getUser();
 
-        if($user->getPartner()->getCodePartner()!=$workshopOrder->getCodePartner()){
+        if(!$security->isGranted('ROLE_SUPER_AD') and $user->getPartner()->getCodePartner()!=$workshopOrder->getCodePartner()){
             return $this->render('TwigBundle:Exception:exception_access.html.twig');
         }
         if ($security->isGranted('ROLE_SUPER_AD')) {
