f43f60f5fb1028b148f8d7a9147830cbb4c57df8
llmartinez <lmartinez@grupeina.com>
2016-10-20 11:37:30 +0200

    Cambios a medias filtro cat_serv  en tienda

--- src/Adservice/UtilBundle/Resources/public/js/catserv.js
+++ src/Adservice/UtilBundle/Resources/public/js/catserv.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
+    var partner = $('#adservice_workshopbundle_workshoptype_partner').val();
+                    
+    // PARTNER
+        $('#adservice_workshopbundle_workshoptype_partner').empty();
 
-	// PARTNER
-	    $('form').find('select[id$=partner]').empty();
+    if($('#flt_catserv').val() != undefined ){
+        populate_partner2(partner);
 
-	// DIAG. MACHINE
-	    $('form').find('select[id$=diagnosis_machines]').empty();
+    }
+    else if($('form').find('select[name*=category_service]') != undefined ){
+        populate_partner2(partner);
 
-	// TIPOLOGY
-	    $('form').find('select[id$=typology]').empty();
-
-
+    }
+            
     //si clickamos el combobox de categoria de servicio rellenamos los relacionados
     $('form').find('select[name*=category_service]').change(function() {
         populate_partner();
