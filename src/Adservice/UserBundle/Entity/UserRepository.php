5b01a12aeb71caaa526dc78fb455440440a1c620
dmaya <dmaya@grupeina.com>
2016-11-09 08:56:30 +0100

    Bugfix buscar usuario por mail

--- src/Adservice/UserBundle/Entity/UserRepository.php
+++ src/Adservice/UserBundle/Entity/UserRepository.php
@@ -44,7 +44,7 @@ class UserRepository extends EntityRepository
      */
     public function findByOption($em, $security, $country, $catserv, $option, $term, $field, $pagination)
     {
-         
+
         $query = 'SELECT u FROM UserBundle:user u JOIN u.user_role r WHERE r.name = :role';
 
         if(!$security->isGranted('ROLE_SUPER_ADMIN')) {
@@ -58,15 +58,15 @@ class UserRepository extends EntityRepository
         if ($catserv != 0 ) {
             $query = $query.' AND u.category_service = '.$catserv;
         }
-        
+
         if (!$field == 0 ) {
             if ($term == 'tel') {
                $query = $query." AND u.phone_number_1 != '0' AND (u.phone_number_1 LIKE '%" . $field . "%' OR u.phone_number_2 LIKE '%" . $field . "%' OR u.mobile_number_1 LIKE '%" . $field . "%' OR u.mobile_number_2 LIKE '%" . $field . "%')";
             } elseif ($term == 'mail') {
-               $query = $query." AND u.email_1 != '0' AND (e.email_1 LIKE '%" . $field . "%' OR e.email_2 LIKE '%" . $field . "%')";
+               $query = $query." AND u.email_1 != '0' AND (u.email_1 LIKE '%" . $field . "%' OR u.email_2 LIKE '%" . $field . "%')";
             } elseif ($term == 'user') {
                $query = $query." AND u.username  LIKE '%" . $field . "%'";
-            } 
+            }
         }
         $consulta = $em ->createQuery($query)
                         ->setParameter('role', $option)
